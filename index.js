642d3cf234782fd1dacda8fde2ffb7dbcf57dd19
Kamrul Hasan Ridoy <ridoyhasan87@gmail.com>
2023-05-19 19:08:30 +0600

    add toys post method

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req,res)=>{
-    res.send('Otaku running')
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kjhm1xo.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
-app.listen(port, ()=>{
-    console.log(`Otaku running on port ${port}`);
-})
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const toyCollection = client.db('toyDB').collection('toy');
+
+    app.post('/toys',async(req,res)=>{
+        const newToy = req.body;
+        console.log(newToy);
+        const result = await toyCollection.insertOne(newToy);
+        res.send(result);
+      })   
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Otaku running");
+});
+app.listen(port, () => {
+  console.log(`Otaku running on port ${port}`);
+});
